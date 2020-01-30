5119789db576c0cc72662f89815064e73798134f
oscarfong <oscar.fong@gmail.com>
2020-01-30 16:39:49 +0800

    Multiline javascript js string

--- Hello.js
+++ Hello.js
@@ -1,3 +1,12 @@
 import React from 'react';
 
 export default ({ name }) => <div><h1>Using nearly parser Hello {name}! </h1><p/><a href="https://medium.com/@gajus/parsing-absolutely-anything-in-javascript-using-earley-algorithm-886edcc31e5e">https://medium.com/@gajus/parsing-absolutely-anything-in-javascript-using-earley-algorithm-886edcc31e5e</a></div>;
+
+
+
+// Multiline javascript js string
+var html_multiline = `
+  <div>
+    <span>Some HTML here</span>
+  </div>
+`;
